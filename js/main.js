cf86a4770d6f8a15b4d1b007ffdda9f14c06bb49
rhysjparry <hello@rhys.jp>
2015-04-07 13:48:31 +0100

    Fixes to the masonry template picker, changed header table centering to flexbox.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-// Initialize Tech Tab Slider
-
-var techswiper = new Swiper('.tech-swiper', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    spaceBetween: 30,
-    autoplay: 5000,
-    autoplayDisableOnInteraction: true,
-    effect: 'fade',
-     paginationBulletRender: function (index, className) {
-        return '<span class="' + className + '">' + '<i class="arrow"></i>' + '</span>';
-    }
-});
-
-
-
-// Initialize Theme Slider
-
-var themeswiper = new Swiper('.showcase-swiper', {
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    effect: 'fade'
-});
-
 
 // Mobile Navigation Toggle
 
