f042a60a7db5c88e6120b61c00d241c1f3dfe7c9
rhysjparry <hello@rhys.jp>
2015-09-07 16:55:13 +0100

    Added pricing page.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@
 $( ".nav-toggle").click(function() {
     $( ".header, .nav-toggle, nav" ).toggleClass( "active" );
 });
+
+
+// Pricing Table Toggle
+
+$( ".free").click(function() {
+    $( ".free .pricing-benefits" ).toggleClass( "open" );
+});
+
+$( ".basic").click(function() {
+    $( ".basic .pricing-benefits" ).toggleClass( "open" );
+});
+
+$( ".highlighted").click(function() {
+    $( ".highlighted .pricing-benefits" ).toggleClass( "open" );
+});
+
+
