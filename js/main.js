86fab18684ee4399359194038644ed00bcd3547f
rhysjparry <rhys.parry@basekit.com>
2016-02-25 09:57:47 +0000

    Added new reseller package to pricing page.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ $( ".basic").click(function() {
     $( ".basic .pricing-benefits" ).toggleClass( "open" );
 });
 
+$( ".business").click(function() {
+    $( ".business .pricing-benefits" ).toggleClass( "open" );
+});
+
 $( ".highlighted").click(function() {
     $( ".highlighted .pricing-benefits" ).toggleClass( "open" );
 });
@@ -24,9 +28,9 @@ $( ".highlighted").click(function() {
 // Pricing Selector
 
 var prices = {
-    "gbp": [0, 7, 12],
-    "usd": [0, 10, 19],
-    "eur": [0, 8, 14]
+    "gbp": [0, 7, 12, 4.60],
+    "usd": [0, 10, 19, 7],
+    "eur": [0, 8, 14, 6]
 }
 
 $('.currency-selector select').change(function () {
@@ -35,16 +39,19 @@ $('.currency-selector select').change(function () {
         $('.free-package').text('$' + prices.usd[0]);
         $('.basic-package').text('$' + prices.usd[1]);
         $('.business-package').text('$' + prices.usd[2]);
+        $('.reseller-package').text('$' + prices.usd[3]);
         break;
     case 'gbp':
         $('.free-package').text('£' + prices.gbp[0]);
         $('.basic-package').text('£' + prices.gbp[1]);
         $('.business-package').text('£' + prices.gbp[2]);
+        $('.reseller-package').text('£' + prices.gbp[3]);
         break;
     case 'eur':
         $('.free-package').text('€' + prices.eur[0]);
         $('.basic-package').text('€' + prices.eur[1]);
         $('.business-package').text('€' + prices.eur[2]);
+        $('.reseller-package').text('€' + prices.eur[3]);
         break;
 	}
 });
